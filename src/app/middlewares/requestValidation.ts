fa117f5d9612328780f41a85e64c4a67bcab2e2f
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-06-14 15:23:49 +0600

    use catchAsync utils in request validation middleware

--- src/app/middlewares/requestValidation.ts
+++ src/app/middlewares/requestValidation.ts
@@ -1,18 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import { AnyZodObject } from "zod";
+import catchAsync from "../utils/catchAsync";
 
 const requestValidation = (schema: AnyZodObject) => {
 
-    return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            await schema.parseAsync({
-                body: req.body
-            })
-        } catch (error) {
-            next(error)
-        }
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        await schema.parseAsync({
+            body: req.body
+        })
         next();
-    }
+    })
 }
 
 export default requestValidation;
