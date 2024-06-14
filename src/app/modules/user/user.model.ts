766740d7925a345fe20d11dcad57da3ff0cff373
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-06-14 13:01:02 +0600

    hashed password before save into db

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,6 +1,9 @@
 import mongoose, { model } from "mongoose";
 import { IUser } from "./user.interface";
+import bcrypt from 'bcrypt'
+import configs from "../../configs";
 
+// user schema
 const userSchema = new mongoose.Schema<IUser>({
     name: {
         type: String,
@@ -28,6 +31,18 @@ const userSchema = new mongoose.Schema<IUser>({
     timestamps: true
 })
 
+// make sure password is hashed before saving to db
+userSchema.pre("save", async function (next) {
+    const user = this as IUser;
+
+    // hash password
+    user.password = await bcrypt.hash(user.password, Number(configs.salt_rounds));
+
+    next();
+})
+
+// user model
 const User = model<IUser>("User", userSchema);
 
 export default User;
+
