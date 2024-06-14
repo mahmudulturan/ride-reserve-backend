99d64ea06fd26c5e8c2c6bf9c2c5ffa9e1ee57ed
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-06-14 15:27:13 +0600

    add: car interface

---
+++ src/app/modules/car/car.interface.ts
@@ -0,0 +1,12 @@
+
+
+export interface ICar {
+    name: string;
+    description: string;
+    color: string;
+    isElectric: boolean;
+    status: 'available' | 'unavailable';
+    features: string[];
+    pricePerHour: number;
+    isDeleted: boolean;
+}
