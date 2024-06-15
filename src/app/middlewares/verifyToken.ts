bf04a804120d7b5edeea04fbc1fe80edba791987
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-06-15 16:20:53 +0600

    add: verifyToken middleware for verify the jwt token

---
+++ src/app/middlewares/verifyToken.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import catchAsync from "../utils/catchAsync";
+import jwt, { JwtPayload, VerifyErrors } from 'jsonwebtoken';
+import configs from "../configs";
+import httpStatus from "http-status";
+import AppError from "../errors/AppError";
+import User from "../modules/user/user.model";
+
+type TRole = 'user' | 'admin';
+
+const verifyToken = (...role: TRole[]) => {
+    console.log(role);
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.headers.authorization?.split(' ')[1];
+
+        if (!token) {
+            throw new AppError(httpStatus.UNAUTHORIZED, 'Unauthorized Access');
+        }
+
+        try {
+            const decoded = jwt.verify(token, configs.access_token_secret as string) as JwtPayload;
+
+            const { userId, role } = decoded;
+
+            const user = await User.findById(userId);
+
+            if (!user) {
+                throw new AppError(httpStatus.NOT_FOUND, "User Not Found");
+            }
+
+            const isMatchedRole = role.includes(role);
+
+            if (!isMatchedRole) {
+                throw new AppError(httpStatus.UNAUTHORIZED, "Unauthorized Access");
+            };
+
+            req.user = decoded as JwtPayload;
+        } catch (error) {
+            throw new AppError(httpStatus.UNAUTHORIZED, "Unauthorized Access");
+        }
+        next();
+    })
+}
+
+export default verifyToken;
--- src/app/modules/car/car.route.ts
+++ src/app/modules/car/car.route.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { carController } from "./car.controller";
 import requestValidation from "../../middlewares/requestValidation";
 import { carValidation } from "./car.validation";
+import verifyToken from "../../middlewares/verifyToken";
 
 const router = Router();
 
 // route for create car
-router.post('/', requestValidation(carValidation.createCarValidationSchema), carController.createCar);
+router.post('/', verifyToken("admin"), requestValidation(carValidation.createCarValidationSchema), carController.createCar);
 
 // router for get all cars
 router.get('/', carController.getAllCars);
