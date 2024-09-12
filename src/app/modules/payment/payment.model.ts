330db640edde71e80de03201b0d69ef2dcd21f42
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-09-12 22:01:47 +0600

    add: payment model

---
+++ src/app/modules/payment/payment.model.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { IPayment } from "./payment.interface";
+
+const modelSchema = new mongoose.Schema<IPayment>({
+    amount: {
+        type: Number,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true
+    },
+    booking: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Booking',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+
+const Payment = mongoose.model<IPayment>('Payment', modelSchema);
+
+
+export default Payment;
